0a489d226103dc92cbafe8ac025071781789fe46
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-05-22 21:28:17 +0600

    add toy added

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-  console.log(process.env.DB_PASS)
-  console.log(process.env.DB_USER)
+   
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b1f0ncj.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,6 +25,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const  toyCollection = client.db('dollToy').collection('addtoys')
+
+    app.get('/alltoys', async(req, res) => {
+        const data = toyCollection.find();
+        const result = await data.toArray()
+        res.send(result)
+    })
+        // Add new toys
+    app.post('/addtoys',async(req,res)=>{
+        const newToy = req.body;
+        console.log(newToy)
+        const result = await toyCollection.insertOne(newToy);
+        res.send(result);
+      })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
