3ae0351cff100643483c36e4c472d1708850c54d
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-05-23 00:29:40 +0600

    all toy added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -40,6 +40,23 @@ async function run() {
         const result = await toyCollection.insertOne(newToy);
         res.send(result);
       })
+      app.get('/alltoys/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)} 
+        const result = await  toyCollection.findOne(query)
+        res.send(result)
+    })
+
+    app.get('/alltoys', async(req,res) =>{
+        console.log(req.query.email)
+        let query = {};
+        if(req.query?.email){
+                query = {email: req.query.email}
+        }
+        const result = await toyCollection.find(query).toArray();
+        res.send(result)
+    })
+
 
 
 
